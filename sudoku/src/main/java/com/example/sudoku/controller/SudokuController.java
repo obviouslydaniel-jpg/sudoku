5abfbf7ced60a6ad287f4697ddee06daefe801ba
Daniel <daniel.escobar.escobar@correounivalle.edu.co>
2025-10-07 21:28:14 -0500

    agregué el tablero 6x6 en SudokuController, hay que corregir el tamaño

--- sudoku/src/main/java/com/example/sudoku/controller/SudokuController.java
+++ sudoku/src/main/java/com/example/sudoku/controller/SudokuController.java
@@ -1,19 +1,34 @@
 package com.example.sudoku.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
 
 public class SudokuController {
 
     @FXML
-    private GridPane grid;
+    private GridPane gridSudoku;
 
     @FXML
     private Button btnStart;
 
     @FXML
     public void initialize() {
-        System.out.println("Controlador listo.");
+        int size =6 ; //tamaño del tablero
+
+        for (int fila = 0; fila < size; fila ++) {
+
+            for (int columna=0; columna <size; columna++){ //dos bucles anidados que recorren las filas y las columnas generando las 36 celdas
+
+                TextField casilla = new TextField();
+                casilla.setPrefWidth(40);
+                casilla.setPrefHeight(40);
+                gridSudoku.add(casilla, columna, fila);
+
+
+            }
+        }
+
     }
 }
